af557260139672e0e736ce5d89185678cc6ef135
liqi <lq@mpig.com.cn>
2017-12-10 21:24:02 +0800

    Add files via upload

---
+++ RRTwithMap.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# This program generates a simple rapidly under an existing map,
+# refering to rrt.py from http://msl.cs.uiuc.edu/~lavalle/code.html.
+
+# The maps come from http://rkala.in/codes.php,
+# Could also get Matlab codes from this web.
+
+# Exploring random tree (RRT) in a 500*500 map
+# and planning path from source to goal.
+#
+# Written by LiQi from Shanghai Maritime University.Dec 2017
+# More details in https://github.com/liqisa/RRT.Python
+
+import random, pygame
+from math import sqrt,cos,sin,atan2
+import matplotlib.image as mpimg
+
+#constants
+XDIM     = 500
+YDIM     = 500
+WINSIZE  = [XDIM, YDIM]
+EPSILON  = 7.0
+NUMNODES = 6000
+source   = [10,10]
+goal     = [490,490]
+
+# get map shape
+map_raw = mpimg.imread('map2.bmp')
+[width,height] = [len(map_raw[0]),len(map_raw[1])]
+
+
+def dist(p1,p2): # get distance
+    return sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]))
+
+def step_from_to(p1,p2):
+    if dist(p1,p2) < EPSILON:
+        return p2
+    else:
+        theta = atan2(p2[1]-p1[1],p2[0]-p1[0])
+        return p1[0] + EPSILON*cos(theta), p1[1] + EPSILON*sin(theta)
+
+
+def main():
+    # initialize and prepare screen
+    pygame.init()
+    screen = pygame.display.set_mode(WINSIZE)
+    pygame.display.set_caption('TEST')
+    map = pygame.image.load('map3.bmp').convert()
+    screen.blit(map,[0,0])
+    pygame.display.flip()
+    blue = [0,0,255]
+
+    nodes = []
+    nodes.append((source[0],source[1])) # Start from node source
+
+    for i in range(NUMNODES):
+        rand = random.randint(0,width-1), random.randint(0,height-1)
+        while map.get_at(rand)[0:3] != (255,255,255):
+            rand = random.randint(0, width-1), random.randint(0,height-1)
+        nn = nodes[0]
+        for p in nodes:
+            if dist(p,rand) < dist(nn,rand):
+                nn = p
+        newnode = step_from_to(nn,rand)
+        while map.get_at((int(round(newnode[0])),int(round(newnode[1]))))[0:3]!=(255,255,255):
+            rand = random.randint(0,width-1), random.randint(0,height-1)
+            while map.get_at(rand)[0:3] != (255, 255, 255):
+                rand = random.randint(0,width-1), random.randint(0,height-1)
+            nn = nodes[0]
+            for p in nodes:
+                if dist(p, rand) < dist(nn, rand):
+                    nn = p
+            newnode = step_from_to(nn, rand)
+
+        nodes.append(newnode)
+        pygame.draw.line(screen,blue,nn,newnode)
+        pygame.display.update()
+        if dist(newnode,goal) <= EPSILON:
+            print newnode,'Found Path'
+            break
+
+# if python says run, then we should run
+if __name__ == '__main__':
+  main()
+  input() # hold on the image
